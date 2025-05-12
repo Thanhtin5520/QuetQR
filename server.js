fd431aad7cbf1802d1b4015f3c24a3aba96619ea
Thanhtin5520 <thanhtin5520@gmail.com>
2025-05-12 17:31:23 +0700

    Cập nhật index.html

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get("/scanner", (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     module.exports = app;
 } else {
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 8080;
     server.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
